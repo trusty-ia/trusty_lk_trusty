f6001eb5cd2bbe3cdb218d99fed71173d0953681
Dima Zavin <dima@android.com>
2013-08-13 11:47:16 -0700

    [lib][syscall] add a helper for defining syscall handlers
    Change-Id: Ia711a986498cc57ef84af79142cd3e8fb870bd4f
    Signed-off-by: Dima Zavin <dima@android.com>

--- lib/syscall/rules.mk
+++ lib/syscall/rules.mk
@@ -5,6 +5,9 @@ MODULE := $(LOCAL_DIR)
 MODULE_SRCS += \
 	$(LOCAL_DIR)/syscall.c
 
+GLOBAL_INCLUDES += \
+	$(LOCAL_DIR)/include
+
 include $(LOCAL_DIR)/arch/$(ARCH)/rules.mk
 
 include make/module.mk
