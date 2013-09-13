ad643fc8377f8884c0596187b55132775546381d
Dima Zavin <dima@android.com>
2013-09-13 11:01:49 -0700

    [trusty] Add IPC syscalls to the syscall table
    Change-Id: I8893b9dab794837defa4b523237511043d28a330
    Signed-off-by: Dima Zavin <dima@android.com>

--- lib/trusty/include/syscall_table.h
+++ lib/trusty/include/syscall_table.h
@@ -32,3 +32,20 @@ DEF_SYSCALL(0x3, exit_group, long, 0)
 DEF_SYSCALL(0x4, read, long, 3, uint32_t fd, void* msg, uint32_t size)
 DEF_SYSCALL(0x5, ioctl, long, 3, uint32_t fd, uint32_t req, void* buf)
 DEF_SYSCALL(0x6, nanosleep, long, 3, uint32_t clock_id, uint32_t flags, uint64_t sleep_time)
+
+/* IPC connection establishement syscalls */
+DEF_SYSCALL(0x10, port_create, int, 4, const char *path, int num_recv_bufs, size_t recv_buf_size, uint32_t flags)
+DEF_SYSCALL(0x11, connect, int, 2, const char *path, unsigned long timeout_msecs)
+DEF_SYSCALL(0x12, accept, int, 1, int handle_id)
+DEF_SYSCALL(0x13, close, void, 1, int handle_id)
+DEF_SYSCALL(0x14, set_cookie, void, 2, int handle, void *cookie)
+
+/* handle polling related syscalls */
+DEF_SYSCALL(0x18, wait, int, 3, int handle_id, uevent_t *event, unsigned long timeout_msecs)
+DEF_SYSCALL(0x19, wait_any, int, 2, uevent_t *event, unsigned long timeout_msecs)
+
+/* message send/recv syscalls */
+DEF_SYSCALL(0x20, get_msg, int, 2, int handle, ipc_msg_info_t *msg_info)
+DEF_SYSCALL(0x21, read_msg, int, 4, int handle, uint32_t msg_id, uint32_t offset, ipc_msg_t *msg)
+DEF_SYSCALL(0x22, put_msg, void, 2, int handle, uint32_t msg_id)
+DEF_SYSCALL(0x23, send_msg, int, 2, int handle, ipc_msg_t *msg)
