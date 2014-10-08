1dc3dfc2fa843212796686f3cadbb44a19d5b206
Arve Hjønnevåg <arve@android.com>
2014-10-08 16:49:11 -0700

    [lib][version] Add version number and buildid to version string if set
    If VERSION_MAJOR, VERSION_MINOR or BUILDID are set, add them to the
    version string.
    
    Change-Id: Ib8a0df9fa496b41f40d21384310ec81de44f06d7

--- lib/version/rules.mk
+++ lib/version/rules.mk
@@ -8,6 +8,21 @@ GLOBAL_INCLUDES += \
 MODULE_SRCS := \
 	$(LOCAL_DIR)/version.c \
 
+ifneq ($(VERSION_MAJOR),)
+MODULE_CFLAGS += \
+	-DVERSION_MAJOR=$(VERSION_MAJOR)
+endif
+
+ifneq ($(VERSION_MINOR),)
+MODULE_CFLAGS += \
+	-DVERSION_MINOR=$(VERSION_MINOR)
+endif
+
+ifneq ($(BUILDID),)
+MODULE_CFLAGS += \
+	-DBUILDID=$(BUILDID)
+endif
+
 include make/module.mk
 
 EXTRA_BUILDRULES += lib/version/version.mk
--- lib/version/version.c
+++ lib/version/version.c
@@ -1,3 +1,24 @@
 #include <version.h>
 
-char lk_version[] = "Built: " __TIME__ " " __DATE__;
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#ifdef VERSION_MINOR
+#define VERSION_MINOR_STR "." TOSTRING(VERSION_MINOR)
+#else
+#define VERSION_MINOR_STR ""
+#endif
+
+#ifdef VERSION_MAJOR
+#define VERSION_STR "Version: " TOSTRING(VERSION_MAJOR) VERSION_MINOR_STR ", "
+#else
+#define VERSION_STR ""
+#endif
+
+#ifdef BUILDID
+#define BUILDID_STR "Build: " TOSTRING(BUILDID) ", "
+#else
+#define BUILDID_STR ""
+#endif
+
+char lk_version[] = VERSION_STR BUILDID_STR "Built: " __TIME__ " " __DATE__;
