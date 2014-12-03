bd4d40f34d193c78a10e5c4725b004609f024fe0
Arve Hjønnevåg <arve@android.com>
2014-12-02 20:23:05 -0800

    [lib][uthread] arm64 UTHREAD_WITH_MEMORY_MAPPING_SUPPORT support
    Change-Id: I92d06b440dc5acefababd3ddd72d80db837989f9

--- lib/uthread/arch/arm64/uthread.c
+++ lib/uthread/arch/arm64/uthread.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013-2014, Google Inc. All rights reserved
+ * Copyright (c) 2012-2013, NVIDIA CORPORATION. All rights reserved
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
@@ -176,3 +177,175 @@ status_t arch_uthread_unmap(struct uthread *ut, struct uthread_map *mp)
 	                       MMU_USER_TOP_SHIFT, MMU_USER_PAGE_SIZE_SHIFT,
 	                       ut->page_table, ut->arch.asid);
 }
+
+#ifdef UTHREAD_WITH_MEMORY_MAPPING_SUPPORT
+
+#define ARM64_WRITE_AT(reg, val) \
+({ \
+    __asm__ volatile("at " #reg ", %0" :: "r" (val)); \
+    ISB; \
+})
+
+/* TEX[1:0] and CB mapping:
+ *
+ * 00	Non-cacheable
+ * 01	Write-back, Write-allocate
+ * 10	Write-through, no Write-allocate
+ * 11	Write-back, no Write-allocate
+ *
+ * MAIR mappings
+ *
+ * 0100		Non-cacheable
+ * 00RW,10RW	Write-through
+ * 01RW,11RW	Write-back
+ *
+ */
+static u_int mair_to_cb [] = {
+	/* 0000 */	0x4,	/* Error! Device memory */
+	/* 0001 */	0x2,	/* write through transient, write allocate */
+	/* 0010 */	0x2,	/* write through transient, read allocate */
+	/* 0011 */	0x2,	/* write through transient, read/write allocate */
+	/* 0100 */	0x0,	/* Non-cacheable */
+	/* 0101 */	0x1,	/* write back transient, write allocate */
+	/* 0110 */	0x3,	/* write back transient, read allocate */
+	/* 0111 */	0x1,	/* write back transient, read/write allocate */
+	/* 1000 */	0x2,	/* write through non-transient */
+	/* 1001 */	0x2,	/* write through non-transient, write allocate */
+	/* 1010 */	0x2,	/* write through non-transient, read allocate */
+	/* 1011 */	0x2,	/* write through non-transient, read/write allocate */
+	/* 1100 */	0x3,	/* write back non-transient */
+	/* 1101 */	0x1,	/* write back non-transient, write allocate */
+	/* 1110 */	0x3,	/* write back non-transient, read allocate */
+	/* 1111 */	0x1,	/* write back non-transient, read/write allocate */
+};
+
+/* Translate vaddr from current context and map into target uthread */
+status_t arch_uthread_translate_map(struct uthread *ut_target,
+				    ext_vaddr_t vaddr_src,
+				    vaddr_t vaddr_target, paddr_t *pfn_list,
+				    uint32_t npages, u_int flags, bool ns_src,
+				    uint64_t *ns_pte_list)
+{
+	u_int pg;
+	ext_vaddr_t vs;
+	vaddr_t vt;
+	pte_t pte_attr;
+	status_t err = NO_ERROR;
+	u_int first_inner = ~0, first_outer = ~0, first_shareable = ~0;
+
+	pte_attr = MMU_PTE_ATTR_NON_GLOBAL | MMU_PTE_ATTR_AF;
+	if (flags & UTM_NS_MEM)
+		pte_attr |= MMU_PTE_ATTR_NON_SECURE;
+	if (flags & UTM_W)
+		pte_attr |= MMU_PTE_ATTR_AP_P_RW_U_RW;
+	else
+		pte_attr |= MMU_PTE_ATTR_AP_P_RO_U_RO;
+
+	vs = vaddr_src;
+	vt = vaddr_target;
+	for (pg = 0; pg < npages; pg++, vs += USER_PAGE_SIZE, vt += USER_PAGE_SIZE) {
+		u_int inner, outer, shareable;
+		uint64_t par;
+		uint64_t physaddr;
+
+		if (ns_src) {
+			/* Get physaddr and attr from the list of ptes
+			 *
+			 * !!! This code only works if ptes are ARM v8
+			 *     VMSAv8-64 level 3 page table entries
+			 *     describing 4K granular pages
+			 * !!!
+			 * TODO: Add support for other page table formats.
+			 */
+			uint64_t pte = ns_pte_list[pg];
+			if ((flags & UTM_W) && !NS_PTE_AP_U_RW(pte)) {
+				dprintf(CRITICAL, "%s: Making a writable mapping "
+						"to NS read-only page!\n", __func__);
+				goto err_out;
+			}
+
+			physaddr = NS_PTE_PHYSADDR(pte);
+
+			dprintf(SPEW, "%s: physaddr 0x%llx cache attributes 0x%llx, pte 0x%llx\n",
+					__func__, physaddr, NS_PTE_ATTR_MAIR(pte), pte);
+
+			outer = mair_to_cb[NS_PTE_ATTR_OUTER(pte)];
+			if (outer & 0x4) {
+				dprintf(CRITICAL, "%s: Unknown outer cache attributes 0x%x\n",
+						__func__, outer);
+				goto err_out;
+			}
+
+			inner = mair_to_cb[NS_PTE_ATTR_INNER(pte)];
+			if (inner & 0x4) {
+				dprintf(CRITICAL, "%s: Unknown inner cache attributes 0x%x\n",
+						__func__, inner);
+				goto err_out;
+			}
+
+			shareable = NS_PTE_ATTR_SHAREABLE(pte);
+		} else {
+			arch_disable_ints();
+			if (flags & UTM_W)
+				ARM64_WRITE_AT(S1E0W, vs);
+			else
+				ARM64_WRITE_AT(S1E0R, vs);
+			par = ARM64_READ_SYSREG(par_el1);
+			arch_enable_ints();
+
+			if (par & PAR_ATTR_FAULTED) {
+				dprintf(CRITICAL,
+					"failed %s user read V2P (v = 0x%016llx, par = 0x%016llx)\n",
+					(flags & UTM_NS_MEM) ? "NS" : "SEC",
+					vs, par);
+				err = ERR_NOT_VALID;
+				goto err_out;
+			}
+
+			inner = PAR_LDESC_ATTR_INNER(par);
+			outer = PAR_LDESC_ATTR_OUTER(par);
+			shareable = PAR_LDESC_ATTR_SHAREABLE(par);
+			physaddr = PAR_LDESC_ALIGNED_PA(par);
+		}
+		/*
+		 * Set flags on first page and check for attribute
+		 * consistency on subsequent pages
+		 */
+		if (pg == 0) {
+			if (shareable)
+				pte_attr |= MMU_PTE_ATTR_SH_INNER_SHAREABLE;
+
+			/* inner cacheable (cb) */
+			/* outer cacheable (tex) */
+			if (inner | outer)
+				pte_attr |= MMU_PTE_ATTR_NORMAL_MEMORY;
+
+			first_inner = inner;
+			first_outer = outer;
+			first_shareable = shareable;
+		} else if (inner != first_inner || outer != first_outer ||
+				shareable != first_shareable) {
+			dprintf(CRITICAL,
+				"%s: attribute inconsistency!\n", __func__);
+			err = ERR_NOT_VALID;
+			goto err_out;
+		}
+		pfn_list[pg] = physaddr;
+
+		if (ns_src) {
+			dprintf(SPEW, "%s: vt 0x%lx, physaddr 0x%llx, select pte_attr 0x%llx\n",
+					__func__, vt, physaddr, pte_attr);
+		}
+
+		err = arm64_mmu_map(vt, pfn_list[pg], USER_PAGE_SIZE, pte_attr,
+		                    0, MMU_USER_SIZE_SHIFT, MMU_USER_TOP_SHIFT,
+		                    MMU_USER_PAGE_SIZE_SHIFT,
+		                    ut_target->page_table, ut_target->arch.asid);
+		if (err != NO_ERROR)
+			goto err_out;
+	}
+
+err_out:
+	return err;
+}
+#endif
