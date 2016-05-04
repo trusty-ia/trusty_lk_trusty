ab3bdaef58a88c5a74ab5420daf6bafa29257c52
Zhong,Fangjian <fangjian.zhong@intel.com>
2016-05-04 13:39:02 +0800

    [lib][sm] Work around to fix simics bug
    Change-Id: I6a627ee1744977f00765afc4cb310e282833e29c
    Signed-off-by: Zhong,Fangjian <fangjian.zhong@intel.com>

--- lib/sm/sm.c
+++ lib/sm/sm.c
@@ -311,6 +311,19 @@ static void sm_wait_for_smcall(void)
 
 		/* Re-enable interrupts (needed for SMC_SC_NOP) */
 		arch_enable_ints();
+
+#if ENABLE_TRUSTY_SIMICS
+		/* Temporary fix for simics bug.
+		 * Simics does not support interrupt windows vemxit;
+		 * LK call cpuid to trigger vmexit, then pending interrupt
+		 * for LK will be injected by eVmm */
+		__asm__ volatile(
+			"xorq %rax, %rax\n"
+			"xorq %rcx, %rcx\n"
+			"cpuid\n"
+		);
+#endif
+
 	}
 }
 
