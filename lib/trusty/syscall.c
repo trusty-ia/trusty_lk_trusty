d282e7f0930576baa0cba3aa0e607b3eb651b06e
Yan, Shaopu <shaopu.yan@intel.com>
2016-11-09 14:21:58 +0800

    [lib][syscall] Add a whitelist for which TA could retrieve seed from kernel.
    Change-Id: If4ee48943fd796da23955cd2f828fbddadf29a28
    Signed-off-by: Yan, Shaopu <shaopu.yan@intel.com>

--- lib/trusty/syscall.c
+++ lib/trusty/syscall.c
@@ -264,6 +264,24 @@ long sys_finish_dma(user_addr_t uaddr, uint32_t size, uint32_t flags)
 
 #endif
 
+static bool valid_ta_to_retrieve_seed()
+{
+	uuid_t white_list[] = {
+		HWCRYPTO_SRV_APP_UUID,
+		KEYMASTER_SRV_APP_UUID
+		/* Add more TAs which are trusted to retrieve the seed */
+		};
+	uint i;
+
+	trusty_app_t *trusty_app = uthread_get_current()->private_data;
+	for (i=0; i<sizeof(white_list)/sizeof(white_list[0]); i++) {
+		/* matches one in the whitelist */
+		if (!memcmp(&trusty_app->props.uuid, &white_list[i], sizeof(uuid_t)))
+			return true;
+	}
+	return false;
+}
+
 /*
  * Based on the design the IMR region for LK will reserved some bytes for ROT
  * and seed storage (size = sizeof(seed_response_t)+sizeof(rot_data_t))
@@ -272,6 +290,9 @@ long sys_get_device_info(user_addr_t * info, bool need_seed)
 {
 	long    ret = 0;
 	trusty_device_info_t *   dev_info = NULL;
+
+	if (!valid_ta_to_retrieve_seed())
+		panic("the caller is invalid!\n");
 
 	if (!info || !g_trusty_startup_info)
 		panic("the params is NULL!\n");
