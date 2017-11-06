26fbfa5167282bfaec6097c504bf00560bafe8e8
Yan, Xiangyang <xiangyangx.yan@intel.com>
2017-11-06 16:07:11 +0800

    [lib][syscall] Move the x86 specified syscall to x86 sepcified folder
    Change-Id: I17e802633c11187a5a2a2aa762df3a97ffeb7b7e
    Signed-off-by: Yan, Xiangyang <xiangyangx.yan@intel.com>

--- lib/trusty/syscall.c
+++ lib/trusty/syscall.c
@@ -281,64 +281,6 @@ long sys_finish_dma(user_addr_t uaddr, uint32_t size, uint32_t flags)
 
 #endif
 
-static bool valid_ta_to_retrieve_seed(void)
-{
-	uuid_t white_list[] = {
-		HWCRYPTO_SRV_APP_UUID,
-		KEYMASTER_SRV_APP_UUID
-		/* Add more TAs which are trusted to retrieve the seed */
-		};
-	uint i;
-
-	trusty_app_t *trusty_app = uthread_get_current()->private_data;
-	for (i=0; i<sizeof(white_list)/sizeof(white_list[0]); i++) {
-		/* matches one in the whitelist */
-		if (!memcmp(&trusty_app->props.uuid, &white_list[i], sizeof(uuid_t)))
-			return true;
-	}
-	return false;
-}
-
-/*
- * Based on the design the IMR region for LK will reserved some bytes for ROT
- * and seed storage (size = sizeof(seed_response_t)+sizeof(rot_data_t))
- */
-long sys_get_device_info(user_addr_t info, bool need_seed)
-{
-	long ret = 0;
-	trusty_device_info_t dev_info;
-
-	if (need_seed && !valid_ta_to_retrieve_seed())
-		panic("the caller is invalid!\n");
-
-	if(g_trusty_startup_info.size_of_this_struct != sizeof(trusty_startup_info_t))
-		panic("trusty_startup_info_t size mismatch!\n");
-
-	/* make sure the shared structure are same in tos loader, LK kernel */
-	if(g_dev_info->size != sizeof(trusty_device_info_t))
-		panic("trusty_device_info_t size mismatch!\n");
-
-	/* memcpy may result to klocwork scan error, so size is checked before memcpy is called. */
-	memcpy(&dev_info, g_dev_info, sizeof(trusty_device_info_t));
-
-	/* for Km1.0 no need the osVersion and patchMonthYear */
-	dev_info.rot.osVersion = 0;
-	dev_info.rot.patchMonthYear = 0;
-
-	if (!need_seed)
-		/* seed is the sensitive secret date, do not return to user app if it is not required. */
-		memset(dev_info.seed_list, 0, sizeof(dev_info.seed_list));
-
-	dev_info.state.data = 0;
-
-	ret = copy_to_user(info, &dev_info, sizeof(trusty_device_info_t));
-	memset(&dev_info, 0, sizeof(dev_info));
-
-	if (ret != NO_ERROR)
-		panic("failed (%ld) to copy structure to user\n", ret);
-
-	return NO_ERROR;
-}
 
 long sys_get_paddr(user_addr_t paddr, user_addr_t uaddr)
 {
